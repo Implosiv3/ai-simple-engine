5b034bb9b7378331464f5ca161ce2e52fdae695e
danialcala94 <danielalcalavalera@gmail.com>
2026-06-28 18:42:20 +0700

    Added new 'RunnerRegistry' class. New version.

---
+++ src/ai_simple_engine/runners/registry/abstract.py
@@ -0,0 +1,38 @@
+from ai_simple_engine.services.keyed_service_registry import KeyedServiceRegistry
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+
+TModel = TypeVar('TModel')
+TRunner = TypeVar('TRunner')
+TKey = TypeVar('TKey')
+
+class RunnerRegistry(
+    KeyedServiceRegistry[TRunner],
+    Generic[TModel, TRunner, TKey],
+    ABC
+):
+    """
+    Base registry capable of resolving the correct
+    runner for a given model.
+    """
+
+    @abstractmethod
+    def key_for(
+        self,
+        model: TModel
+    ) -> TKey:
+        """
+        Returns the registry key that should be used
+        for the supplied model.
+        """
+        ...
+
+    def resolve(
+        self,
+        model: TModel
+    ) -> TRunner:
+        """
+        Resolves the appropriate runner for a model.
+        """
+        return self.get(self.key_for(model))
