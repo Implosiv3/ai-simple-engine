343bda2afc7f8b8c2446aca53d503932a797deec
danialcala94 <danielalcalavalera@gmail.com>
2026-06-28 16:31:04 +0700

    Added 'Runner' abstract class. New version.

---
+++ src/ai_simple_engine/runners/abstract.py
@@ -0,0 +1,11 @@
+from abc import ABC
+
+
+class Runner(
+    ABC
+):
+    """
+    Base class for all inference runners that
+    will execute the code to actually perform
+    an operation.
+    """
